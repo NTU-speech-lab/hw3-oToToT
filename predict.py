35dfc50ab55f4834bbae8c83f363f095efee59b5
oToToT <ty1208chiang@gmail.com>
2020-04-13 00:13:22 +0800

    update best model

--- predict.py
+++ predict.py
@@ -12,25 +12,24 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset, ConcatDataset
 import time
 
-# torch.cuda.device(3)
-
+IMAGE_SIZE = 112
 def readfile(path, label):
     # label 是一個 boolean variable，代表需不需要回傳 y 值
     image_dir = sorted(os.listdir(path))
-    x = np.zeros((len(image_dir), 128, 128, 3), dtype=np.uint8)
+    x = np.zeros((len(image_dir), IMAGE_SIZE, IMAGE_SIZE, 3), dtype=np.uint8)
     y = np.zeros((len(image_dir)), dtype=np.uint8)
     for i, file in enumerate(image_dir):
         img = cv2.imread(os.path.join(path, file))
-        # resize to 128 x ? or ? x 128
+        # resize to IMAGE_SIZE x ? or ? x IMAGE_SIZE
         height = img.shape[0]
         width = img.shape[1]
-        rate = 128 / max(height, width)
+        rate = IMAGE_SIZE / max(height, width)
         height = int(height * rate)
         width = int(width * rate)
         img = cv2.resize(img, (width, height))
         # pad black
         # from https://blog.csdn.net/qq_20622615/article/details/80929746
-        W, H = 128, 128
+        W, H = IMAGE_SIZE, IMAGE_SIZE
         top = (H - height) // 2
         bottom = (H - height) // 2
         if top + bottom + height < H:
@@ -50,24 +49,89 @@ def readfile(path, label):
       return x
 
 
+# In[4]:
+
+
+# 分別將 training set、validation set、testing set 用 readfile 函式讀進來
 try:
     workspace_dir = sys.argv[1]
 except:
     workspace_dir = './food-11'
+print("Reading data")
+train_x, train_y = readfile(os.path.join(workspace_dir, "training"), True)
+print("Size of training data = {}".format(len(train_x)))
+val_x, val_y = readfile(os.path.join(workspace_dir, "validation"), True)
+print("Size of validation data = {}".format(len(val_x)))
 test_x = readfile(os.path.join(workspace_dir, "testing"), False)
 print("Size of Testing data = {}".format(len(test_x)))
 
+
+# # Dataset
+# 在 PyTorch 中，我們可以利用 torch.utils.data 的 Dataset 及 DataLoader 來"包裝" data，使後續的 training 及 testing 更為方便。
+# 
+# Dataset 需要 overload 兩個函數：\_\_len\_\_ 及 \_\_getitem\_\_
+# 
+# \_\_len\_\_ 必須要回傳 dataset 的大小，而 \_\_getitem\_\_ 則定義了當程式利用 [ ] 取值時，dataset 應該要怎麼回傳資料。
+# 
+# 實際上我們並不會直接使用到這兩個函數，但是使用 DataLoader 在 enumerate Dataset 時會使用到，沒有實做的話會在程式運行階段出現 error。
+# 
+
+# In[5]:
+
+
+# training 時做 data augmentation
+train_transform1 = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomChoice([
+        transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomPerspective()
+    ]),
+    transforms.RandomChoice([
+        transforms.RandomAffine(10), # 隨機線性轉換
+        transforms.RandomRotation(40)
+    ]),
+    transforms.ColorJitter(), # 隨機色溫等
+    transforms.ToTensor(), # 將圖片轉成 Tensor，並把數值 normalize 到 [0,1] (data normalization)
+#     transforms.Normalize(
+#         [77.89311144813877 / 255, 102.3587941606983 / 255, 126.59376063616554 / 255],
+#         [72.80305392379675 / 255, 75.35438507973123 / 255, 79.31408066842762 / 255]
+#     )
+])
+train_transform2 = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomOrder([
+        transforms.RandomChoice([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomPerspective()
+        ]),
+        transforms.RandomAffine(30), # 隨機線性轉換
+        transforms.RandomResizedCrop((IMAGE_SIZE, IMAGE_SIZE), scale=(0.5, 1.0)), # 隨機子圖
+    ]),
+    transforms.RandomChoice([
+        transforms.ColorJitter(), # 隨機色溫等
+        transforms.RandomGrayscale(),
+    ]),
+    transforms.ToTensor(), # 將圖片轉成 Tensor，並把數值 normalize 到 [0,1] (data normalization)
+    transforms.RandomErasing(0.2),
+#     transforms.Normalize(
+#         [77.89311144813877 / 255, 102.3587941606983 / 255, 126.59376063616554 / 255],
+#         [72.80305392379675 / 255, 75.35438507973123 / 255, 79.31408066842762 / 255]
+#     )
+])
+# testing 時不需做 data augmentation
 test_transform = transforms.Compose([
     transforms.ToPILImage(),                                    
     transforms.ToTensor(),
-    transforms.Normalize(
-        [77.89311144813877 / 255, 102.3587941606983 / 255, 126.59376063616554 / 255],
-        [72.80305392379675 / 255, 75.35438507973123 / 255, 79.31408066842762 / 255]
-    )
+#     transforms.Normalize(
+#         [77.89311144813877 / 255, 102.3587941606983 / 255, 126.59376063616554 / 255],
+#         [72.80305392379675 / 255, 75.35438507973123 / 255, 79.31408066842762 / 255]
+#     )
 ])
 class ImgDataset(Dataset):
     def __init__(self, x, y=None, transform=None):
         self.x = x
+        # label is required to be a LongTensor
         self.y = y
         if y is not None:
             self.y = torch.LongTensor(y)
@@ -84,64 +148,81 @@ class ImgDataset(Dataset):
         else:
             return X
 
+
+# In[6]:
+
+
+batch_size = 64
+train_set = ConcatDataset([
+    ImgDataset(train_x, train_y, train_transform1),
+    ImgDataset(train_x, train_y, train_transform2)
+])
+val_set = ImgDataset(val_x, val_y, test_transform)
+train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=16)
+val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=16)
+
+
+# # Model
+
+# In[7]:
+
+
 class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()
+        # torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
+        # torch.nn.MaxPool2d(kernel_size, stride, padding)
+        # input 維度 [3, IMAGE_SIZE, IMAGE_SIZE]
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 128, 3, 1, 1),  # [64, 128, 128]
+            nn.Conv2d(3, 128, 3, 1, 1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
+            nn.MaxPool2d(2, 2, 0),
             
-            nn.Dropout2d(0.5),
-
-            nn.Conv2d(128, 128, 3, 1, 1), # [128, 64, 64]
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
-
-            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
+            nn.Conv2d(128, 256, 3, 1, 1),
             nn.BatchNorm2d(256),
-            nn.PReLU(1),
-            nn.MaxPool2d(2, 2, 0),      # [256, 16, 16]
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
             
-            nn.Dropout2d(0.1),
-
-            nn.Conv2d(256, 512, 3, 1, 1), # [512, 16, 16]
+            nn.Conv2d(256, 512, 3, 1, 1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),       # [512, 8, 8]
-
-            nn.Conv2d(512, 512, 3, 1, 1), # [512, 8, 8]
+            nn.Conv2d(512, 512, 3, 1, 1),
             nn.BatchNorm2d(512),
             nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),       # [512, 4, 4]
+            nn.MaxPool2d(2, 2, 0),
+            
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, 3, 1, 1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2, 0),
         )
         self.fc = nn.Sequential(
-            nn.Linear(512*4*4, 512),
+            nn.Linear(7*7*512, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(),
             
-            nn.Linear(512, 256),
+            nn.Linear(1024, 1024),
+            nn.BatchNorm1d(1024),
             nn.ReLU(),
             
-            nn.Linear(256, 128),
-            nn.Dropout(0.3),
-            nn.ReLU(),
-            
-            nn.Linear(128, 100),
-            nn.ReLU(),
-            
-            nn.Linear(100, 30),
-            nn.PReLU(1),
-
-            nn.Linear(30, 11)
+            nn.Linear(1024, 11)
         )
 
     def forward(self, x):
         out = self.cnn(x)
         out = out.view(out.size()[0], -1)
         return self.fc(out)
-
+ 
 
 model_best = torch.load('model.torch')
 print('model loaded')
--- predict.sh
+++ predict.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget 'https://ototot.tk/cloud/ML/hw3.torch' -O model.torch
+# from https://github.com/NTU-speech-lab/hw3-oToToT/releases/tag/v0.0.6
+wget 'https://drive.google.com/uc?export=download&id=1VvQonwOyuk1lTaJOoUx-Vdy6xPBfC80z' -O model.torch
 
 python3 predict.py $1 $2
